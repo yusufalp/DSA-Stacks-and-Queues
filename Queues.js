7e18e38d85765cfbb5b367f4bf3b58425fb1f453
Yusuf Alp <alpyusuf007@gmail.com>
2020-11-02 09:43:42 -0800

    Added some functions

--- Queues.js
+++ Queues.js
@@ -1,7 +1,10 @@
+const Stack = require('./Stacks');
+
 class _Node {
   constructor(value) {
     this.value = value;
     this.next = null;
+    this.prev = null;
   }
 }
 
@@ -45,7 +48,30 @@ class Queue {
   }
 }
 
-const starTrekQ = new Queue;
+class DoubleQueue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+  }
+
+  enqueue(data) {
+    const node = new _Node(data);
+
+    // if there is no queue
+    if (this.first === null) {
+      this.first = node
+    }
+
+    if (this.last) {
+      this.last.next = node
+      node.prev = this.last
+    }
+
+    this.last = node
+  }
+}
+
+const starTrekQ = new DoubleQueue;
 
 starTrekQ.enqueue('Kirk')
 starTrekQ.enqueue('Spock')
@@ -53,24 +79,86 @@ starTrekQ.enqueue('Uhura')
 starTrekQ.enqueue('Sulu')
 starTrekQ.enqueue('Checkov')
 
-function peek(queue){
-  if(queue.first){
+function peek(queue) {
+  if (queue.first) {
     return queue.first.value
   }
 }
 // console.log(peek(starTrekQ))
 
-function isEmpty(queue){
+function isEmpty(queue) {
   return queue.first === null ? true : false
 }
 // console.log(isEmpty(starTrekQ))
 
-function display(queue){
+function display(queue) {
   let current = queue.first
-  while(current){
+  while (current) {
     console.log(current.value)
     current = current.next
   }
   return current
 }
+
 // console.log(display(starTrekQ))
+// console.log(starTrekQ.last)
+
+function displayBack(queue) {
+  let current = queue.last
+  while (current) {
+    console.log(current.value)
+    current = current.prev
+  }
+}
+
+// console.log(displayBack(starTrekQ))
+
+function ophidianBank(queue) {
+  let queueNum = Math.ceil(Math.random() * 4)
+  console.log(queueNum)
+  // 1 means paperwork is not done
+  if (queueNum === 1) {
+    console.log(`${queue.first.value} does not have paperwork`)
+    queue.last.next = queue.first
+    queue.first = queue.first.next
+    queue.last.next.next = null
+    queue.last = queue.last.next
+  } else {
+    console.log(`${queue.first.value} is served`)
+    queue.first = queue.first.next
+  }
+  return queue
+}
+
+// console.log('result-->', ophidianBank(starTrekQ))
+
+const alpha = new Stack();
+
+alpha.push('a')
+alpha.push('b')
+alpha.push('c')
+alpha.push('d')
+alpha.push('e')
+
+console.log(alpha)
+
+function removeItem(queue) {
+  const temp = new Stack();
+
+  let current = queue.pop()
+  while (current) {
+    console.log('value',current)
+    if(queue.top){
+      temp.push(current)
+    }
+    current = queue.pop()
+  } 
+  current = temp.pop()
+  while (current) {
+    queue.push(current)
+    current = temp.pop()
+  }
+  return queue
+}
+
+console.log(removeItem(alpha).print())
