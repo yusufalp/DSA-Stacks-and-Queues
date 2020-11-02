6382af5b74aec0860b7fd46cf19987556a91146f
Yusuf Alp <alpyusuf007@gmail.com>
2020-11-02 09:44:05 -0800

    Added some functions to solve problems pointed

--- Stacks.js
+++ Stacks.js
@@ -28,13 +28,16 @@ class Stack {
     // point the pointer to the next item
     // then next item becomes the top of the stack.
     const node = this.top;
+    if (!node){
+      return 
+    }
     this.top = node.next;
     return node.data;
   }
 
-  print(){
+  print() {
     let current = this.top
-    while(current){
+    while (current) {
       console.log(current.data)
       current = current.next
     }
@@ -73,15 +76,88 @@ function is_palindrome(s) {
   s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
 
   let stringStacked = new Stack;
-  for (let letter of s){
+  for (let letter of s) {
     stringStacked.push(letter)
-  } 
+  }
 
   let reversedString = ''
-  for (let i=0; i<s.length; i++){
+  for (let i = 0; i < s.length; i++) {
     reversedString += stringStacked.pop()
   }
   return reversedString === s
 }
 
 // console.log(is_palindrome("A man, a plan, a canal: Panama"));
+
+
+function matchParenthesis(str) {
+  let newStack = new Stack();
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '(' || str[i] === '[' || str[i] === '{') {
+      newStack.push(str[i])
+    }
+    if (str[i] === ')') {
+      let poped = newStack.pop()
+      if (poped !== '(') {
+        return false
+      }
+    }
+    if (str[i] === ']') {
+      let poped = newStack.pop()
+      if (poped !== '[') {
+        return false
+      }
+    }
+    if (str[i] === '}') {
+      let poped = newStack.pop()
+      if (poped !== '{') {
+        return false
+      }
+    }
+  }
+  if (newStack.top === null) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// console.log(matchParenthesis('([{}])'))
+// console.log(matchParenthesis('([{})'))
+
+let unsorted = new Stack();
+
+unsorted.push('Kirk')
+unsorted.push('Spock')
+unsorted.push('McCoy')
+unsorted.push('Scotty')
+unsorted.push('Alp')
+unsorted.push('Yusuf')
+
+// console.log(unsorted.print())
+
+function sortStack(unsorted) {
+  let temp = new Stack();
+  let topNode = unsorted.pop()
+  while(topNode && !unsorted.top){
+    console.log(topNode)
+    if (topNode > unsorted.top.data){
+      let node = unsorted.pop()
+      temp.push(node)
+    } else {
+      unsorted.push(topNode)
+      let node = temp.pop()
+      while(node){
+        unsorted.push(node)
+        node = temp.pop()
+      }
+      topNode = unsorted.pop()
+    }
+  }
+  console.log(unsorted.print())
+  console.log(temp.print())
+}
+
+// console.log(sortStack(unsorted))
+
+module.exports = Stack;
